957025e5bab0b9748eb28400d3ac6245fd3cac5b
longls231 <49274147+viettran0801@users.noreply.github.com>
2022-01-25 22:55:58 +0700

    add enviroment

--- internal/infrastructure/db.go
+++ internal/infrastructure/db.go
@@ -1,14 +1,22 @@
 package infrastructure
 
 import (
+	"fmt"
 	"gofiber/domain"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectToDB() (*gorm.DB, error) {
-	dns := "host=db user=postgres password=password dbname=testdb port=5432"
+	dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+
 	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 	if err != nil {
 		return nil, err
